989d7ea374f8e5d17f8ac6efc45805d93bb950e8
anushabandari <anusha6624@gmail.com>
2020-02-13 15:03:12 -0800

    merging two arrays

---
+++ MergeTwoArrays.js
@@ -0,0 +1,20 @@
+function Merge(nums1,nums2)
+{
+  var nums3=[];
+
+    nums3=nums1.concat(nums2);
+
+    return nums3;
+    
+}
+
+
+function MergeArray(nums1,nums2)
+{
+  var nums3=[];
+
+    nums3=nums1.concat(nums2).sort();
+
+    return nums3;
+    
+}
