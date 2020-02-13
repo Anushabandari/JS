25aeb2144c318ea246b4f9eab4ea9f7a444d94b1
anushabandari <anusha6624@gmail.com>
2020-02-13 12:31:51 -0800

    MajorityElement

---
+++ Majority.js
@@ -0,0 +1,31 @@
+function Majority(arr)
+{
+    var Key=new Map();
+    var MostTimes=0;
+    var MajorElement;
+
+    for(var i=0;i<arr.length;i++)
+    {
+        if( !Key.has(arr[i]))
+        {
+            Key.set(arr[i],1)
+             
+        }
+        else
+        {
+            Key.set(arr[i],Key.get(arr[i]) +1)
+        }
+
+    }
+    for( var [k,v] of Key.entries())
+    {
+        debugger;
+        if(MostTimes< v)
+        {
+            MostTimes=v;
+            MajorElement= k;
+        }
+    }
+     return [MajorElement,MostTimes];
+
+}
